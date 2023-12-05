a82f0c141d0f9af97bbe9b652dd32e79097a711e
Lachlan <107536990+LachlanCallcott@users.noreply.github.com>
2023-12-05 18:22:44 +1000

    Javascript_Started_(needs-debugging)

---
+++ To_Do_List/script.js
@@ -0,0 +1,11 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "excersise",
+    "study Javascript",
+    "eat healthy"
+];
+
+for (var i=0; i< todos.length; i++) {
+   todos[i].pop();
+}
